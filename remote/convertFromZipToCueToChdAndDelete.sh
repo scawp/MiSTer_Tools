a5e08f18a767f2a3f9144ebe336494fe3af1069a
scawp <rob@scawp.com>
2026-07-30 17:48:16 +0200

    Update convertFromZipToCueToChdAndDelete.sh
    Added batchsize arg
    Added safe arg

--- remote/convertFromZipToCueToChdAndDelete.sh
+++ remote/convertFromZipToCueToChdAndDelete.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 
-for file in *.zip; do 
-  7za x -aoa "$file" -o"${file%.*}"
-  RESULT=$?
-  if [ $RESULT -eq 0 ]; then
-	chdman createcd -i "./${file%.*}/${file%.*}.cue" -o "${file%.*}.chd" --force
-	RESULT=$?
-	if [ $RESULT -eq 0 ]; then
-	  echo "Deleting zip $file"
-	  rm "$file"
-	  echo "Deleting folder ./${file%.*}/"
-	  rm -r "./${file%.*}/"
-	else
-      echo "Chd'ing failed! Skipping delete step!"
-	fi
-  else
-    echo "Unzip failed! Skipping delete step and chd'ing!"
+batchSize=0 #unlimited
+#startsWith="" #TODO: Maybe...
+doNotDelete=0 #delete original zip files and gemerated cue/bins
+
+for param in "$@"; do
+  if [[ $param =~ ^[0-9]+$ ]]; then
+    batchSize="$param"
+  fi
+  #if [[ $param =~ ^[a-Z]$ ]]; then
+  #  startsWith="${param^^}"
+  #fi
+  if [[ "${param,,}" = "safe" ]]; then
+    doNotDelete=1
   fi
 done
 
-echo "Fin!"
+for file in *.zip; do 
+  7za x -aoa "$file" -o"${file%.*}"
+  retVal=$?
+  if [ $retVal -eq 0 ]; then
+    chdman createcd -i "./${file%.*}/${file%.*}.cue" -o "${file%.*}chd" --force
+    retVal=$?
+    if [ $retVal -eq 0 ]; then
+      if [ $doNotDelete -eq 0 ]; then
+      echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Deleting zip $file"
+      rm "$file"
+      echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Deleting folder ./${file%.*}/"
+      rm -r "./${file%.*}/"
+      else
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Do not delete flag active, leaving files!"
+      fi
+    else
+      echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Chd'ing failed! Skiping delete step!"
+    fi
+  else
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Unzip failed! Skiping delete step and chd'ing!"
+  fi
+  if [ $batchSize -gt 0 ]; then
+    if [ $batchSize -eq 1 ]; then
+      echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Batch Complete! Quitting!"
+      exit 0;
+    else
+    ((batchSize=batchSize-1))
+      echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>$batchSize to go in this batch!"
+    fi
+  fi
+done
